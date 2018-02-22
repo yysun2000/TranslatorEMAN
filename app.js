2575e3255daa4412c9c545bddfcf551a663b247b
cocacolar <yysun2000@nate.com>
2018-02-22 16:13:49 +0900

    20180222 DOCKER_retry2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const global = require("./global.js")
 const translate = require("./translate");
 const web = require("./web")
 
-web.listen(4000,(req,res)=>{
+web.listen(2424,(req,res)=>{
   /*
   translate({inputfilename:"./test/한글.xlsx",jsonfilename:"./한글_result.json"})*/
   console.log("DONE!");
